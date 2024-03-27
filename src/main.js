da7ee847f115d2ec1009f04baf31f0c4c0af71b3
overH <2262135195@qq.com>
2024-03-27 11:23:33 +0800

    elementPlus采用sass样式配置系统

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
 
+
 const app = createApp(App)
 app.use(ElementPlus)
 app.use(createPinia())
--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,6 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -19,13 +18,24 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      resolvers: [ElementPlusResolver({ importStyle: "sass"})],
     }),
+
   ],
   resolve: {
     // 实际的路径转换 @ ->src
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `,
+      }
     }
   }
 })
