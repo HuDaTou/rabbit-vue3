f546875101e716630900c09482c0a6a596f26217
overH <2262135195@qq.com>
2024-03-28 21:39:59 +0800

    vue自定义指令的实现案例懒加载

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import router from './router'
 
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import { useIntersectionObserver } from '@vueuse/core'
 
 // 引入初始化文件
 import '@/styles/common.scss'
@@ -20,3 +21,22 @@ app.use(router)
 
 
 app.mount('#app')
+
+// 定义全局指令
+app.directive('img-lazy', {
+    mounted (el, binding) {
+        // el：指令绑定的元素 .eg img
+        // binding：bingding.value 指令等于号后面绑定的表达式的值
+        // console.log(el,binding.value);
+        useIntersectionObserver(
+            el,
+            ([{ isIntersecting }], ) => {
+             console.log(isIntersecting);
+             if (isIntersecting) {
+                // 进入视口区域
+                el.src = binding.value
+             }
+            },
+          )
+    }
+})
