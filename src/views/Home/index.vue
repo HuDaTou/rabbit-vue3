3b50bc804a55f713ae2643be10ee689ed7a0b787
overH <2262135195@qq.com>
2024-03-28 17:54:48 +0800

    Home 面板组件封装

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,6 +4,7 @@ import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
 import homeProduct from './components/HomeProduct.vue'
+import HomePanel from './components/HomePanel.vue'
 </script>
 
 <template>
@@ -14,4 +15,20 @@ import homeProduct from './components/HomeProduct.vue'
   <HomeNew />
   <HomeHot />
   <homeProduct />
+
+  <!-- 测试面板组件 -->
+  <!-- 纯展示类组件通用封装思路总结：
+  1.搭建纯静态的部分，不管可变的部分
+  2.抽象可变的部分为组件参数非复杂的模版抽象成props，复杂的结构模版抽象为插槽 -->
+
+  <HomePanel title="新鲜好物" sub-title="新鲜好物 好多商品">
+<div>
+  我是新鲜好物插槽内容
+</div>
+  </HomePanel>
+  <HomePanel title="人气推荐" sub-title="人气推荐 好多商品">
+    <div>
+      我是人气推荐插槽内容
+    </div>
+  </HomePanel>
 </template>
