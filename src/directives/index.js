de9fe82b0db7e91feda0200c561d6301ca92d8dd
overH <2262135195@qq.com>
2024-03-28 21:52:27 +0800

    懒加载优化;将懒加载自定义指令封装为插件再通过app注册为全局指令

---
+++ src/directives/index.js
@@ -0,0 +1,26 @@
+import { useIntersectionObserver } from '@vueuse/core'
+
+export const lazyPlugin = {
+    install (app) {
+        // 定义全局指令
+app.directive('img-lazy', {
+    mounted (el, binding) {
+        // el：指令绑定的元素 .eg img
+        // binding：bingding.value 指令等于号后面绑定的表达式的值
+        // console.log(el,binding.value);
+        useIntersectionObserver(
+            el,
+            // isIntersecting的作用是进入视口区域为true的布尔值
+            ([{ isIntersecting }], ) => {
+                
+             console.log(isIntersecting);
+             if (isIntersecting) {
+                // 进入视口区域
+                el.src = binding.value
+             }
+            },
+          )
+    }
+})
+    }
+}
