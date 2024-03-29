88d29745b328351d02e89ac3b413d9b8aa29d93f
overH <2262135195@qq.com>
2024-03-29 18:17:25 +0800

    加上 : 号就好了，这个有什么用

--- src/views/Home/components/HomeProduct.vue
+++ src/views/Home/components/HomeProduct.vue
@@ -18,32 +18,10 @@ onMounted(() => {
   <div class="home-product">
     <HomePanel :title="cate.name" v-for="cate in goodsProduct" :key="cate.id">
       <div class="box">
-        <RouterLink class="cover" to="/">
-          <img v-img-lazy="cate.picture" />
-          <strong class="label">
-            <span>{{ cate.name }}馆</span>
-            <span>{{ cate.saleInfo }}</span>
-          </strong>
-        </RouterLink>
+        <Goodscate :cate="cate"/>
         <ul class="goods-list">
           <li v-for="goods in cate.goods" :key="goods.id">
-            <RouterLink to="/" class="goods-item">
-              <img :src="goods.picture" alt="" />
-              <p class="name ellipsis">{{ goods.name }}</p>
-              <p class="desc ellipsis">{{ goods.desc }}</p>
-              <p class="price">&yen;{{ goods.price }}</p>
-            </RouterLink>
-          </li>
-        </ul>
-      </div>
-    </HomePanel>
-
-    <HomePanel :title="cate.name" v-for="cate in goodsProduct" :key="cate.id">
-      <div class="box">
-        <Goodscate cate="cate"/>
-        <ul class="goods-list">
-          <li v-for="goods in cate.goods" :key="goods.id">
-            <Goodsitem goods="goods"/> 
+            <Goodsitem :goods="goods"/> 
           </li>
         </ul>
       </div>
