6a57fec755459e1f2baa6b1fa97a2e9446b3f720
overH <2262135195@qq.com>
2024-04-01 15:31:25 +0800

    一级分类-激活状态显示以及分类列表渲染

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -3,6 +3,7 @@ import { getCategoryAPI } from "@/api/category";
 import { ref, onMounted } from "vue";
 import { useRoute } from "vue-router";
 import { getBannerAPI } from "@/api/Home";
+import GoodsItem from '@/views/Home/components/Goodsitem.vue';
 
 // 获取轮播图数据
 const BannerList = ref([])
@@ -14,7 +15,7 @@ const getBanner = async () => {
   BannerList.value = result
 }
 
-// 获取数据
+// 获取分类的数据
 const categoryData = ref({})
 const route = useRoute()
 
@@ -47,7 +48,28 @@ onMounted(() => {
             <img :src="item.imgUrl" alt="">
           </el-carousel-item>
         </el-carousel>
+      
       </div>
+
+      <div class="sub-list">
+  <h3>全部分类</h3>
+  <ul>
+    <li v-for="i in categoryData.children" :key="i.id">
+      <RouterLink to="/">
+        <img :src="i.picture" />
+        <p>{{ i.name }}</p>
+      </RouterLink>
+    </li>
+  </ul>
+</div>
+<div class="ref-goods" v-for="item in categoryData.children" :key="item.id">
+  <div class="head">
+    <h3>- {{ item.name }}-</h3>
+  </div>
+  <div class="body">
+    <GoodsItem v-for="good in item.goods" :goods="good" :key="good.id" />
+  </div>
+</div>
     </div>
   </div>
 </template>
